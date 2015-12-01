b903fc72a92cb7fb4916da739e2513db3ebda731
Matt <mhk4g@virginia.edu>
2015-11-30 20:47:10 -0500

    Slightly improved appearance

--- getWord.php
+++ getWord.php
@@ -32,21 +32,3 @@ $rows = $result->num_rows;
  endif;
 
 ?>
-<?
-/*
- $query = "select word from Words order by rand() limit 1";
- $result = $db->query($query);
- $rows = $result->num_rows;
- if ($rows >= 1):
-    header('Content-type: text/xml');
-    echo "<?xml version='1.0' encoding='utf-8'?>";
-    echo "<Word>";
-    $row = $result->fetch_array();
-    $ans = $row["word"];
-    echo "<value>$ans</value>";
-    echo "</Word>";
- else:
-    die ("DB Error");
- endif;
- */
-?>
